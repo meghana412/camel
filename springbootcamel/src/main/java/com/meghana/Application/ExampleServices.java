c54e5f8bcb01c33504f708f241bb5bbb4586d91e
meghana412 <kagita12@gmail.com>
2018-04-03 22:13:39 -0500

    Add files via upload

---
+++ springbootcamel/src/main/java/com/meghana/Application/ExampleServices.java
@@ -0,0 +1,8 @@
+package com.meghana.Application;
+
+public class ExampleServices {
+    public static void example(MyBean bodyIn) {
+        bodyIn.setName( "Hello, " + bodyIn.getName() );
+        bodyIn.setId(bodyIn.getId()*10);
+    }
+}
